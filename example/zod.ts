0c449b59db151038f9df604e341e4c630a67a5a2
m1212e <14091540+m1212e@users.noreply.github.com>
2025-07-06 21:07:36 +0200

    🚧 wip: use more precise typemap imports

--- example/zod.ts
+++ example/zod.ts
@@ -1,5 +1,5 @@
 import z from "zod";
-import { defaultSources, konfigure } from "../lib";
+import { defaultSources, konfigure, sources } from "../lib";
 
 const configObject = await konfigure({
 	// instead of typebox you can use zod or valibot
@@ -7,7 +7,13 @@ const configObject = await konfigure({
 		foo: z.string(),
 		bar: z.number().optional(),
 	}),
-	sources: defaultSources,
+	sources: [
+		...defaultSources,
+		sources.object({
+			foo: "fallback",
+			bar: "3",
+		}),
+	],
 });
 
 // reads the foo and bar values from the environment or docker secret files
--- lib/konfigure.ts
+++ lib/konfigure.ts
@@ -1,5 +1,6 @@
 import { Value } from "@sinclair/typebox/value";
-import { type Static, TypeBox } from "@sinclair/typemap";
+import type { Static } from "@sinclair/typemap";
+import { IsTypeBox, IsValibot, IsZod } from "@sinclair/typemap/guard";
 import type { Source } from "./sources/type";
 
 /**
@@ -8,20 +9,55 @@ import type { Source } from "./sources/type";
  * @template Schema - The schema type used for decoding.
  * @param {Schema} params.schema - The schema definition for validation and decoding.
  * @param {Source[]} params.sources - An array of sources to retrieve key-value pairs.
- * @param {string} [params.delimeter="_"] - The delimiter used to convert keys into nested objects. Use "disabled" to disable object mapping.
+ * @param {string} [params.delimiter="_"] - The delimiter used to convert keys into nested objects. Use "disabled" to disable object mapping.
  * @returns - The decoded and transformed values according to the schema.
  */
 
 export async function konfigure<Schema extends object | string>({
 	schema,
 	sources,
-	delimeter = "_",
+	delimiter = "_",
 }: {
 	schema: Schema;
 	sources: Source[];
-	delimeter?: string | "disabled";
+	delimiter?: string | "disabled";
 }) {
-	const convertedSchema = TypeBox(schema);
+	let convertedSchema: any;
+
+	const importErrors = [];
+
+	if (IsTypeBox(schema)) {
+		convertedSchema = schema;
+	}
+
+	if (!convertedSchema && IsZod(schema)) {
+		try {
+			const { TypeBoxFromZod } = await import(
+				"@sinclair/typemap/typebox/typebox-from-zod.mjs"
+			);
+			convertedSchema = TypeBoxFromZod(schema as any);
+		} catch (error: any) {
+			importErrors.push(error);
+		}
+	}
+	if (!convertedSchema && IsValibot(schema)) {
+		try {
+			const { TypeBoxFromValibot } = await import(
+				"@sinclair/typemap/typebox/typebox-from-valibot.mjs"
+			);
+			convertedSchema = TypeBoxFromValibot(schema as any);
+			console.log("called2");
+		} catch (error: any) {
+			importErrors.push(error);
+		}
+	}
+
+	if (!convertedSchema) {
+		throw new Error(`Failed to import typebox from zod, valibot or typebox.
+Please install one of the validators.
+
+${importErrors.map((e) => JSON.stringify(e, null, 2)).join("\n")}`);
+	}
 
 	const values = {};
 	const resolverErrors = [];
@@ -37,7 +73,8 @@ export async function konfigure<Schema extends object | string>({
 		}
 	}
 	let processedValues =
-		delimeter === "disabled" ? values : convertFromDelimeter(values, delimeter);
+		delimiter === "disabled" ? values : convertWithDelimiter(values, delimiter);
+
 	Value.Clean(convertedSchema, processedValues);
 	processedValues = Value.Convert(convertedSchema, processedValues) as any;
 	Value.Default(convertedSchema, processedValues);
@@ -64,8 +101,8 @@ ${resolverErrors.map((e) => JSON.stringify(e, null, 2)).join("\n")}
 }
 
 /**
- * Given an object with keys that are delimeter-separated strings, returns a new
- * object with the same values but with nested objects instead of delimeter-separated
+ * Given an object with keys that are delimiter-separated strings, returns a new
+ * object with the same values but with nested objects instead of delimiter-separated
  * strings as keys.
  *
  * @example
@@ -74,7 +111,7 @@ ${resolverErrors.map((e) => JSON.stringify(e, null, 2)).join("\n")}
  * 	"foo.baz": "qux",
  * };
  *
- * const output = convertFromDelimeter(input, ".");
+ * const output = convertWithDelimiter(input, ".");
  *
  * // output is {
  * // 	foo: {
@@ -83,27 +120,30 @@ ${resolverErrors.map((e) => JSON.stringify(e, null, 2)).join("\n")}
  * // 	},
  * // };
  */
-function convertFromDelimeter(object: Record<string, any>, delimeter: string) {
-	const result: Record<string, any> = {};
+function convertWithDelimiter(object: Record<string, any>, delimiter: string) {
+	const result: any = {};
 
 	for (const [key, value] of Object.entries(object)) {
-		const pathSegments = key.split(delimeter);
+		const keys = key.split(delimiter);
+		let curr = result;
 
-		if (pathSegments.length === 1) {
-			result[pathSegments[0]!] = value;
-			continue;
-		}
-
-		let current = result;
-		for (const segment of pathSegments.slice(0, -1)) {
-			if (!current[segment]) {
-				current[segment] = {};
-				current = { ...current[segment] };
+		for (let i = 0; i < keys.length; i++) {
+			const k = keys[i];
+			if (i === keys.length - 1) {
+				curr[k] = value;
 			} else {
-				current = { ...current[segment] };
+				// Always create a new object if one doesn't exist (or is not a plain object)
+				if (
+					!(k in curr) ||
+					typeof curr[k] !== "object" ||
+					curr[k] === null ||
+					Object.isFrozen(curr[k])
+				) {
+					curr[k] = {};
+				}
+				curr = curr[k];
 			}
 		}
-		current[pathSegments[pathSegments.length - 1]!] = value;
 	}
 	return result;
 }
